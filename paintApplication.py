7418b49039a956b0fde7bddcdaff94280684196f
Chanupa deshan <chanupadeshan2002@gmail.com>
2024-06-14 18:58:05 +0530

    change font size

--- paintApplication.py
+++ paintApplication.py
@@ -19,7 +19,7 @@ topic_label = ttk.Label(root, text="Paint Application", font=("Helvetica", 50, "
 topic_label.pack(side=tk.TOP,pady=10)  
 
 #create label for the color select
-color_label = ttk.Label(root, text="Choose color:", font=(12),background="#D3D3D3")
+color_label = ttk.Label(root, text="Choose color:", font=(12),background="#D3D3D3",foreground="#12205c")
 color_label.pack()
 
 #crate combobox for the color select
@@ -27,7 +27,7 @@ color_pick = ttk.Combobox(root, values=["red", "blue", "green", "yellow", "black
 color_pick.pack(pady=10)
 
 #create label for the size select
-size_label = ttk.Label(root, text="Choose size:", font=(12),background="#D3D3D3",foreground="black")
+size_label = ttk.Label(root, text="Choose size:", font=(12),background="#D3D3D3",foreground="#12205c")
 size_label.pack()
 
 #crate entry for the size select
@@ -43,7 +43,7 @@ def update_size_and_color():
     color_pick_value.set(color_pick.get())
 
 style = ttk.Style()
-style.configure('Custom.TButton', background='#D3D3D3')
+style.configure('Custom.TButton', background='#D3D3D3',foreground='#12205c')
 
 #crate button for the size select
 enter_button = ttk.Button(button_frame, text="Enter", command=update_size_and_color,style='Custom.TButton')
@@ -58,7 +58,7 @@ clear_button = ttk.Button(button_frame, text="Clear", command=clear,style='Custo
 clear_button.pack(side=tk.RIGHT,padx=10)
 
 #crate canvas 
-canvas = tk.Canvas(root, bg="white", height=500, width=900)
+canvas = tk.Canvas(root, bg="white", height=450, width=900)
 canvas.pack(pady=10)
 
 # Function to draw on the canvas
